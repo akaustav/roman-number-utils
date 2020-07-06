4c7f886be0e690b6b26a117d3347db3207638647
Ameet Kaustav <akaustav@users.noreply.github.com>
2020-07-05 23:27:47 -0700

    Roman to Natural

---
+++ src/roman-map.ts
@@ -0,0 +1,9 @@
+export const romanMap = new Map();
+
+romanMap.set('I', 1);
+romanMap.set('V', 5);
+romanMap.set('X', 10);
+romanMap.set('L', 50);
+romanMap.set('C', 100);
+romanMap.set('D', 500);
+romanMap.set('M', 1000);
---
+++ src/roman-to-natural.ts
@@ -0,0 +1,31 @@
+import { romanMap } from './roman-map.ts';
+
+export const romanToNatural = (romanNumber: string): number => {
+  let natural: number = 0;
+
+  if (romanNumber) {
+    const upperRomanNumber = romanNumber.toUpperCase();
+
+    for (let i = 0; i < upperRomanNumber.length; i++) {
+      const currentRomanDigit = upperRomanNumber.charAt(i);
+      const currentDigitValue = romanMap.get(currentRomanDigit);
+
+      if (currentDigitValue === undefined) {
+        throw `Invalid roman digit '${currentRomanDigit}' found in roman number '${romanNumber}'. Acceptable digits are ${[ ...romanMap.keys() ]}`;
+      }
+
+      const nextRomanDigit = upperRomanNumber.charAt(i + 1);
+      const nextDigitValue = romanMap.get(nextRomanDigit);
+
+      if (currentDigitValue < nextDigitValue) {
+        natural -= currentDigitValue;
+      } else {
+        natural += currentDigitValue;
+      }
+    }
+  } else {
+    throw `Invalid roman number '${romanNumber}'`;
+  }
+
+  return natural;
+};
